cdc321b87f622228b89d3d8a5298ca2a9e029e27
Oliwier Michalik <oliwiermichalik@Oliwiers-MacBook-Pro.local>
2025-06-22 12:26:17 +0200

    tests: include actions workflow for simple CI

--- build.zig
+++ build.zig
@@ -1,15 +1,18 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    // Setup options.
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    // Add modules.
     const lib_mod = b.createModule(.{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
 
+    // --< Start Playground >--
     const exe_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
@@ -17,7 +20,9 @@ pub fn build(b: *std.Build) void {
     });
 
     exe_mod.addImport("reportz_lib", lib_mod);
+    // --< End Playground >--
 
+    // Create artifacts.
     const lib = b.addLibrary(.{
         .linkage = .static,
         .name = "reportz",
@@ -44,19 +49,13 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
+    // Tests.
     const lib_unit_tests = b.addTest(.{
         .root_module = lib_mod,
     });
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
-    const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
-    test_step.dependOn(&run_exe_unit_tests.step);
 }
